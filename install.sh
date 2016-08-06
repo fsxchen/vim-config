718ec32b71de54d947d7f27d6da7aebabb8129dd
fsxchen <fsxchen@gmail.com>
2016-08-06 11:04:36 +0800

    new install sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "source ~/.vim/global.vim" >> .vimrc
 echo "source ~/.vim/vundle.vim" >> .vimrc
 echo "source ~/.vim/custom/python.vim" >> .vimrc
+echo "source ~/.vim/custom/c.vim" >> .vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git Vundle
 
